7409a43c85b1405d9b2df57f7fe857bf2d2a6882
Dmitry Kulikov <kulikovdn@gmail.com>
2016-01-12 23:40:24 +0600

    ArrayHelper added.

---
+++ tests/ArrayHelperTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace kdn\cpanel\api;
+
+/**
+ * Class ArrayHelperTest.
+ * Based on code taken from Yii framework.
+ * @package kdn\cpanel\api
+ */
+class ArrayHelperTest extends TestCase
+{
+    /**
+     * @covers kdn\cpanel\api\ArrayHelper::merge
+     * @small
+     * @link http://www.yiiframework.com/
+     * @copyright Copyright (c) 2008 Yii Software LLC
+     * @license http://www.yiiframework.com/license/
+     */
+    public function testMerge()
+    {
+        $a = [
+            'name' => 'Yii',
+            'features' => [],
+        ];
+        $b = [
+            'version' => '1.0',
+            'options' => [
+                'namespace' => false,
+                'unitTest' => false,
+            ],
+            'features' => [
+                'mvc',
+            ],
+        ];
+        $c = [
+            'version' => '1.1',
+            'options' => [
+                'unitTest' => true,
+            ],
+            'features' => [
+                'gii',
+            ],
+        ];
+        $d = [
+            'version' => '2.0',
+            'options' => [
+                'namespace' => true,
+            ],
+            'features' => [
+                'debug',
+            ],
+        ];
+        $result = ArrayHelper::merge($a, $b, $c, $d);
+        $expected = [
+            'name' => 'Yii',
+            'version' => '2.0',
+            'options' => [
+                'namespace' => true,
+                'unitTest' => true,
+            ],
+            'features' => [
+                'mvc',
+                'gii',
+                'debug',
+            ],
+        ];
+        $this->assertEquals($expected, $result);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace kdn\cpanel\api;
+
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Class TestCase.
+ * @package kdn\cpanel\api
+ */
+class TestCase extends PHPUnit_Framework_TestCase
+{
+}
